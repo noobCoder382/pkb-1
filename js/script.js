3538671dc87f79fa5df442ef51b8d33ae9357cf7
ShreyashR <shreyashakhil@gmail.com>
2020-10-13 22:21:34 +0530

    Priyanshi Website

---
+++ js/script.js
@@ -0,0 +1,393 @@
+const STORE = {
+ questions: {
+  FuturePlans: [
+   {
+    question: "How is Shreyash gonna earn",
+    code: ``,
+    answers: [
+     "PLacements",
+     "Mechanic",
+     "Priyanshi ka dahej",
+     "Auto driver"
+    ],
+    correctAnswer: "PLacements"
+	}],
+	nightCravings: [{
+    question: "raat ko 3 bajey jab bahut zyada bhookh lagey, tab kya khaana chahiye",
+    code: ``,
+    answers: [
+     "Horlicks",
+     "Complan",
+     "BournVita",
+     "Milk powder"
+    ],
+    correctAnswer: "BournVita"
+   }],
+   rozKaStruggle: [{
+    question: "How many alarms does priyanshi nees to wake up??",
+    code: ``,
+    answers: [
+     "1",
+     "5",
+     "10",
+     "kitna bhi lagalo na ho paaega"
+    ],
+    correctAnswer: "kitna bhi lagalo na ho paaega"
+   }],
+   khaufBoltey: [{
+    question: "Which of the following is Priyanshi's nightmare",
+    code: ``,
+    answers: [
+     "Not finding the right clothes for an event",
+     "Drawing a straight line in ED Exam",
+     "Arranging her clothes in almirah",
+     "Cooking"
+    ],
+    correctAnswer: "Drawing a straight line in ED Exam"
+   }],
+   shaukApneyApney: [{
+    question: "Who in this world can burn Maggie to charcoal while making it",
+    code: ``,
+    answers: [
+     "Priyanshi",
+     "Priyanshi op",
+     "Masterchef Priyanshi",
+     "Shreyash Royzada"
+    ],
+    correctAnswer: "Masterchef Priyanshi"
+   }],
+   tananareeKpizzaLageySexy: [{
+    question: "What is a good option",
+    code: ``,
+    answers: [
+     "Skipping dominos to go to pizza hut",
+     "Skipping gym to eat a pizza",
+     "Skipping class to go to eat pizza",
+     "All of the above"
+    ],
+    correctAnswer: "All of the above"
+   }]
+ }
+};
+
+
+// Make the quiz. Create a model for our app's state
+function makeQuiz(){
+	// Creating an object to store the app's state when beginning the quiz
+	return {
+		// Gathering a random question out of the available questions for each category
+		questions: helpers.getRandomQuestions(STORE),
+		// Boolean for if the quiz is in progress or not
+		midQuiz: false,
+		// Array of correct/incorrect answers to use for our progress bar
+		progress: {
+			progressBar: [],
+			incorrectCategories: []
+		},
+		// Boolean to determine if the end state should display
+		completed: false,
+		// Monitoring which question we are currently on
+		currentQuestion: 0,
+		// Keeps track of total correct answers
+		correctAnswers: 0,
+		// Keeps current selected answer
+		currentAnswer: "",
+		// Keeps track of % completed
+		percCorrect: 0
+	}
+}
+
+// Apply fadeOut animations and set the stage for DOM text/element changes
+function $fade(appState){
+
+	// This is the completed state of of a quiz
+	if(appState.completed){
+		
+		// Fade out elements with a promise to avoid choppy behavior
+		$.when($('.question-answer-wrapper, .question-wrapper, .answer-wrapper').fadeOut(500))
+			.done(function(){
+
+				// Display results of the quiz
+				$showResults(appState);
+				$('.results-wrapper').hide().removeClass('hide');
+				$('.question-answer-wrapper, .results-wrapper').fadeIn(500);
+	    });
+
+	// This is if the app is just starting
+	} else if(appState.midQuiz === false){
+
+		// Set a flag that the app has begun
+		appState.midQuiz = true;
+		// Fade out elements with a promise to avoid choppy behavior
+		$.when($('.question-answer-wrapper, .question-wrapper, .code, .answer-wrapper, .start-quiz, .quit-quiz, .results-wrapper, .progress, .progress-bar').fadeOut(500))
+			.done(function(){
+				// Remove any progress from a previous quiz (if any);
+				helpers.updateProgressBar(appState);
+				$('.progress-count').html('1 / 10');
+				$('.progress-perc').html('');
+				// Kill previous results
+				$('.failures').remove();
+				// Lots to do... mostly just setting up a new environment for a new quiz
+				$updateQuestion(appState);
+				$('progress-bar').empty();
+				$('progress-fill').html('Progress: <span class="progress-count">1 / 10</span><span class="progress-perc"></span>');
+				$('.question-answer-wrapper, .answer-wrapper').removeClass('begin');
+				$('.submit-btn, .progress, .progress-bar').removeClass('hide');
+				$('.progress, .progress-bar').hide();
+				$('.question-answer-wrapper, .question-wrapper, .answer-wrapper, .progress, .progress-bar').fadeIn(500);
+				if(!$('.code').hasClass('hide')){$('.code').fadeIn(500)};
+	    });
+
+	// This is if the app is in the middle of execution
+	} else if(appState.midQuiz){
+		$.when($('.question-answer-wrapper, .question-wrapper, .answer-wrapper').fadeOut(500))
+			.done(function(){
+				$updateQuestion(appState);
+				$('.question-answer-wrapper, .question-wrapper, .answer-wrapper').fadeIn(500);
+	    });
+	}
+}
+
+// Work in progress...
+function $showResults(appState){
+	if(appState.correctAnswers === 10){
+		let endMsg = `You got ${appState.percCorrect}% correct!`;
+		let endFeedback = `Badhaiyan Badhaiyan Badhaiyan! Janamdin ki gaadha gaadha badhaai!!!!!!!!!!!!!, we don't have anything for you here!`;
+		
+		$('.quiz-end-score').html(endMsg);
+		$('.quiz-end-feedback-p').html(endFeedback);
+	} 
+	else {
+		$('.answer-btn').remove();
+		let endMsg = `You got ${appState.percCorrect}% correct!`
+		let endFeedback = `Badhai ho Mummy You won! here is the secret link :p -`;
+		if(appState.progress.incorrectCategories.length === 0){
+			endFeedback = `Badhaiyan Badhaiyan Badhaiyan! Janamdin ki gaadha gaadha badhaai!!!!!!!!!!!!!, we don't have something for you here!<a href="www.priyanshisharma.me">Chalo dekhey kya hai!!!!</a>`;
+		} else {
+			endFeedback = `You Lost Priyanshi Ji!!!!!!! chalo kuch aur bhi dekhey "<a href = "https://priyanshisharma.me" >kya hai???????</a>"`;
+		}
+		
+		$('.quiz-end-score').html(endMsg);
+		let $failList = $('<ul class="failures"></ul>');
+		appState.progress.incorrectCategories.map((cat => {
+			$failList.append("<li class='category'>" + cat + "</li>");
+		}));
+		$('.quiz-end-feedback-p').html(endFeedback);
+		// $('.quiz-end-categories').append($failList);
+		// $('.results-wrapper').removeClass('hide').css('display', 'flex');
+		// $('.question-answer-wrapper, .results-wrapper, .quiz-end-feedback, .quiz-end-score, .retry-btn').css('display', 'flex').fadeIn(500);		
+	} 
+}
+
+// Update the question, code, answers, buttons in the DOM while we're in a faded out state
+function $updateQuestion(appState){
+	$('.answer-btn').remove();
+
+	// Update the question and code text with the current question
+	$('.question').html(appState.questions[appState.currentQuestion].question);
+
+	// This is a hack to hide/show the code portion
+	// Only 1/3 of the questions contain code so we hide it if they aren't present
+	if(appState.questions[appState.currentQuestion].code == ``){
+		$('.code').addClass('hide');
+	} else {
+		$('.code').removeClass('hide');
+		$('.code').html(`<pre>${appState.questions[appState.currentQuestion].code}</pre>`);
+	}
+	
+	// Change continue back to submit
+	$('.continue-btn')
+		.val("Submit")
+		.removeClass('continue-btn')
+		.addClass('submit-btn')
+		.prop('disabled', true);
+
+	// Add in available answers for the question
+	let $answers = [];
+
+	// Adding the answers to a temporary array
+	for(let i=0; i<appState.questions[appState.currentQuestion].answers.length; i++){
+
+		// Add current question answers to an array
+		let $answer = $('<button class="answer-btn" type="button"></button>');
+		$answer.html(appState.questions[appState.currentQuestion].answers[i]);
+		$answers.push($answer);
+	}
+
+	// Shuffle the answers
+	helpers.shuffleAnswers($answers)
+
+	// Push answers to the DOM
+	$answers.forEach((answer) => {
+		$('.answer-wrapper').prepend(answer);
+	});
+}
+
+// Simple class and enable/disable DOM selection when answer is selected
+function selectAnswer(answer){
+	$('.answer-btn').removeClass('selected');
+	answer.addClass('selected');
+	$('.submit-btn').prop('disabled', false);
+}
+
+// Answer is selected and submitted
+// Push a feedback state
+function submitAnswer(appState){
+
+	// This will be returned true or false based on their answer
+	let correct;
+
+	// Add styles to the answers to show if their answer was correct or not
+	$('.answer-btn').each(function () {
+		if($(this).html() === appState.questions[appState.currentQuestion].correctAnswer){
+			$(this).addClass('pass');
+			// If answer is correct and selected...
+			if($(this).hasClass('selected')){
+				correct = "pass";
+				appState.correctAnswers++;
+				appState.questions[appState.currentQuestion];
+			}
+
+		// Handle correct answer if selected answer is incorrect
+		} else if ($(this).hasClass('selected')){
+			$(this).addClass('fail dim-answer');
+			correct = "fail";
+			appState.progress.incorrectCategories.push(appState.questions[appState.currentQuestion].category);
+
+		// Dim the other answers to make the correct one more prevalent
+		} else {
+			$(this).addClass('dim-answer');
+		}
+	});
+
+	// Add a progress bar indicator to our appState object
+	appState.progress.progressBar.push(`<div class="progress-indicator ${correct}"></div>`);
+	
+	// Update our percent correct (parse a float and set it to a fixed percentage)
+	appState.percCorrect = parseFloat(appState.correctAnswers / (appState.currentQuestion + 1) * 100).toFixed();
+
+	// Update our current question VS total quiz length
+	$('.progress-count').html(`
+		${appState.currentQuestion + 1} / ${appState.questions.length}
+	`);
+
+	// Update our current correct percentage
+	$('.progress-perc').html(`
+		 // ${(appState.percCorrect)}% Correct
+	`)
+
+	// Change submit back to continue
+	$('.submit-btn')
+		.val("Continue")
+		.removeClass('submit-btn')
+		.addClass('continue-btn')
+
+	// Disable selecting answers
+	$('.answer-btn').prop("disabled", true);
+
+	// Update our progress-bar DOM
+	helpers.updateProgressBar(appState);
+
+	// Continue to next question
+	appState.currentQuestion++;
+
+	// Verify if we're done or not
+	if(appState.currentQuestion === appState.questions.length){
+		appState.completed = true;
+	}
+	
+}
+
+// Silly easter egg for saying you don't want to do the quiz
+function killQuiz(){
+	$('.start-quiz, .quit-quiz').hide();
+	let failureMsg = "Umm, you think we are gonna let you quit?? It is not possible, these are easy questions,and to be honest this button was a bug but we are presenting it as a feature, khair this website may seem familiar, but still attempt to banta hai!!!!!"
+	let msgSplit = failureMsg.split(" ");
+	let counter = 0;
+	$('.question').empty();
+	let startTroll = setInterval(function () {
+		$('.question').append(msgSplit[counter] + " ");
+		counter++;
+		if(counter > msgSplit.length - 1){
+  			clearInterval(startTroll);
+  			$('.start-quiz').text('Toh chaliye waapis shuru kartey hain!!').fadeIn(500);
+		}
+	}, 250);
+}
+
+// These are simple algorithms to modify data that don't need an individual function
+let helpers = {
+	// Pick a random question from the available ones
+	pickRandomQ: function(obj,section){
+		return Math.floor(Math.random() * obj.questions[section].length);		
+	},
+	// Gather a random question from every category
+	getRandomQuestions: function(obj){
+		let questions = [];
+		let categories = Object.keys(obj.questions);
+		categories.forEach((cat) => {
+			let randomQ = this.pickRandomQ(obj,cat);
+			let question = obj.questions[cat][randomQ];
+			question.category = cat;
+			questions.push(question);
+		});
+		return questions;
+	},
+	// Shuffle the answers so they don't appear in the same order
+	shuffleAnswers: function(arr){
+	    for (var i = arr.length - 1; i > 0; i--) {
+	        var j = Math.floor(Math.random() * (i + 1));
+	        var temp = arr[i];
+	        arr[i] = arr[j];
+	        arr[j] = temp;
+	    }
+	},
+	// Update progress bar DOM element
+	updateProgressBar: function(appState){
+		$('.progress-bar').empty();
+		appState.progress.progressBar.forEach((progInd => {
+			$('.progress-bar').append(progInd);
+		}))
+	}
+}
+var bgno = 1;
+// Lets start the show
+$(function(){
+
+	// Kill any form refresh
+	$('.answer-wrapper').on('submit', function(e){
+		e.preventDefault();
+	});
+
+	// Storage for quiz app state
+	let quizData;
+
+	// Start quiz
+	$('.start-quiz, .retry-btn').on('click', function(){
+		quizData = makeQuiz();
+		$fade(quizData);
+	});
+
+	// Select an answer
+	$('.question-answer-wrapper').on('click', '.answer-btn', function(){
+		selectAnswer($(this));
+	})
+
+	// Submit your answer to display feedback and advance question counter
+	$('.question-answer-wrapper').on('click', '.submit-btn', function(e){
+		submitAnswer(quizData);
+	});
+
+	// Submit your answer to display feedback and advance question counter
+	$('.question-answer-wrapper').on('click', '.continue-btn', function(){
+		var url_img = "url('img/background"+bgno+".jpg')";
+		document.body.style.backgroundImage = url_img;
+		$fade(quizData);
+		bgno=bgno+1;
+	});
+
+	// Easter egg to chastize the user
+	$('.quit-quiz').on('click', function(){
+		killQuiz();
+	})
+})
